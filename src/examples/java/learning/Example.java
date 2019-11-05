75cd8f695c45f496aaa8a1191328ba32a061dc23
Phillip van Heerden <vanheerden.phillip@gmail.com>
2019-11-05 11:20:35 +0200

    Woot woot

--- src/examples/java/learning/Example.java
+++ src/examples/java/learning/Example.java
@@ -1,50 +1,330 @@
 package learning;
 
+import java.util.Arrays;
+
 import za.ac.sun.cs.coastal.Symbolic;
 
 public class Example {
 
-	public static boolean foo(int[] input) {
+	public static boolean target(char[] A) {
 		int state = 0;
-		int idx = 0;
-
-		while (idx <= 2) {
+		for (int idx = 0; idx < A.length; idx++) {
+			char current = A[idx];
 			if (state == 0) {
-				if (input[idx] >= 51 && input[idx] <= 100) {
-					if (idx == 0 && input[1] == 999 && input[2] == 999) {
-						return false;
-					}
+				if ((current == (char)97)) {
 					state = 1;
-				} else {
-					state = 0;
+					continue;
 				}
-			} else if (state == 1) {
-				if (input[idx] >= 0 && input[idx] <= 20) {
-					state = 2;
-				} else {
-					return false;
+				if ((current >= (char)0 && current <= (char)96) || (current >= (char)98 && current <= (char)65535)) {
+					state = 5;
+					continue;
 				}
-			} else if (state == 2) {
-				return (input[idx] >= 0 && input[idx] <= 20);
 			}
-			idx++;
+			if (state == 1) {
+				if ((current >= (char)0 && current <= (char)96) || (current >= (char)99 && current <= (char)65535)) {
+					state = 5;
+					continue;
+				}
+				if ((current == (char)98)) {
+					state = 3;
+					continue;
+				}
+				if ((current == (char)97)) {
+					state = 2;
+					continue;
+				}
+			}
+			if (state == 2) {
+				if ((current >= (char)0 && current <= (char)96) || (current >= (char)98 && current <= (char)65535)) {
+					state = 5;
+					continue;
+				}
+				if ((current == (char)97)) {
+					state = 2;
+					continue;
+				}
+			}
+			if (state == 3) {
+				if ((current == (char)99)) {
+					state = 4;
+					continue;
+				}
+				if ((current >= (char)0 && current <= (char)98) || (current >= (char)100 && current <= (char)65535)) {
+					state = 5;
+					continue;
+				}
+			}
+			if (state == 4) {
+				if ((current >= (char)0 && current <= (char)65535)) {
+					state = 5;
+					continue;
+				}
+			}
+			if (state == 5) {
+				if ((current >= (char)0 && current <= (char)65535)) {
+					state = 5;
+					continue;
+				}
+			}
 		}
-		return (state == 0);
+		if ((state == 0) || (state == 1) || (state == 2) || (state == 4) || false) { 
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public static boolean parse(char[] A) {
+		int state = 4;
+		for (int idx = 0; idx < A.length; idx++) {
+			char current = A[idx];
+			if (state == 0) {
+				if ((current >= (char)0 && current <= (char)96) || (current >= (char)99 && current <= (char)65535)) {
+					state = 5;
+					continue;
+				}
+				if ((current == (char)98)) {
+					state = 2;
+					continue;
+				}
+				if ((current == (char)97)) {
+					state = 1;
+					continue;
+				}
+			}
+			if (state == 1) {
+				if ((current >= (char)0 && current <= (char)96) || (current >= (char)98 && current <= (char)65535)) {
+					state = 5;
+					continue;
+				}
+				if ((current == (char)97)) {
+					state = 1;
+					continue;
+				}
+			}
+			if (state == 2) {
+				if ((current >= (char)0 && current <= (char)98) || (current >= (char)100 && current <= (char)65535)) {
+					state = 5;
+					continue;
+				}
+				if ((current == (char)99)) {
+					state = 3;
+					continue;
+				}
+			}
+			if (state == 3) {
+				if ((current >= (char)0 && current <= (char)65535)) {
+					state = 5;
+					continue;
+				}
+			}
+			if (state == 4) {
+				if ((current >= (char)0 && current <= (char)96) || (current >= (char)98 && current <= (char)65535)) {
+					state = 5;
+					continue;
+				}
+				if ((current == (char)97)) {
+					state = 0;
+					continue;
+				}
+			}
+			if (state == 5) {
+				if ((current >= (char)0 && current <= (char)65535)) {
+					state = 5;
+					continue;
+				}
+			}
+		}
+		if ((state == 0) || (state == 1) || (state == 3) || (state == 4) || false) { 
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	public static boolean candidate1(int[] input) {
+	
+	public static boolean parse3(char[] A) {
+		int state = 2;
+		for (int idx = 0; idx < A.length; idx++) {
+			char current = A[idx];
+			if (state == 0) {
+				if ((current == (char)99)) {
+					state = 1;
+					continue;
+				}
+				if ((current >= (char)0 && current <= (char)98) || (current >= (char)100 && current <= (char)65535)) {
+					state = 4;
+					continue;
+				}
+			}
+			if (state == 1) {
+				if ((current >= (char)0 && current <= (char)65535)) {
+					state = 4;
+					continue;
+				}
+			}
+			if (state == 2) {
+				if ((current >= (char)0 && current <= (char)96) || (current >= (char)98 && current <= (char)65535)) {
+					state = 4;
+					continue;
+				}
+				if ((current == (char)97)) {
+					state = 3;
+					continue;
+				}
+			}
+			if (state == 3) {
+				if ((current >= (char)0 && current <= (char)96) || (current >= (char)99 && current <= (char)65535)) {
+					state = 4;
+					continue;
+				}
+				if ((current == (char)97)) {
+					state = 1;
+					continue;
+				}
+				if ((current == (char)98)) {
+					state = 0;
+					continue;
+				}
+			}
+			if (state == 4) {
+				if ((current >= (char)0 && current <= (char)65535)) {
+					state = 4;
+					continue;
+				}
+			}
+		}
+		if ((state == 1) || (state == 2) || (state == 3) || false) { 
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	
+	public static boolean parse2(char[] A) {
+		int state = 0;
+		for (int idx = 0; idx < A.length; idx++) {
+			char current = A[idx];
+			if (state == 0) {
+				if ((current >= (char)0 && current <= (char)96) || (current >= (char)98 && current <= (char)65535)) {
+					state = 4;
+					continue;
+				}
+				if ((current == (char)97)) {
+					state = 2;
+					continue;
+				}
+			}
+			if (state == 1) {
+				if ((current >= (char)0 && current <= (char)65535)) {
+					state = 4;
+					continue;
+				}
+			}
+			if (state == 2) {
+				if ((current >= (char)0 && current <= (char)97) || (current >= (char)99 && current <= (char)65535)) {
+					state = 4;
+					continue;
+				}
+				if ((current == (char)98)) {
+					state = 3;
+					continue;
+				}
+			}
+			if (state == 3) {
+				if ((current == (char)99)) {
+					state = 1;
+					continue;
+				}
+				if ((current >= (char)0 && current <= (char)98) || (current >= (char)100 && current <= (char)65535)) {
+					state = 4;
+					continue;
+				}
+			}
+			if (state == 4) {
+				if ((current >= (char)0 && current <= (char)65535)) {
+					state = 4;
+					continue;
+				}
+			}
+		}
+		if ((state == 0) || (state == 1) || (state == 2) || false) { 
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+
+	public static boolean parse1(char[] A) {
+		int state = 0;
+		for (int idx = 0; idx < A.length; idx++) {
+			char current = A[idx];
+			if (state == 0) {
+				if ((current >= (char)0 && current <= (char)96) || (current >= (char)98 && current <= (char)65535)) {
+					state = 1;
+					continue;
+				}
+				if ((current == (char)97)) {
+					state = 0;
+					continue;
+				}
+			}
+			if (state == 1) {
+				if ((current >= (char)0 && current <= (char)65535)) {
+					state = 1;
+					continue;
+				}
+			}
+		}
+		if ((state == 0) || false) { 
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	
+	public static boolean parse0(char[] A) {
+		int state = 0;
+		for (int idx = 0; idx < A.length; idx++) {
+			char current = A[idx];
+			if (state == 0) {
+				if ((current >= (char)0 && current <= (char)65535)) {
+					state = 1;
+					continue;
+				}
+			}
+			if (state == 1) {
+				if ((current >= (char)0 && current <= (char)65535)) {
+					state = 1;
+					continue;
+				}
+			}
+		}
+		if ((state == 0) || false) { 
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean check(char[] input) {
+		boolean targetResult = target(input);
+		boolean learntResult = parse(input);
+		if (targetResult != learntResult) {
+			Symbolic.mark("Counter Example: " + Arrays.toString(input));
+			return false;
+		}
+		
 		return true;
-	}
-
-	public static boolean check(int[] input) {
-		boolean resultCandidate = candidate1(input);
-		boolean resultFoo = foo(input);
-		return resultCandidate == resultFoo;
+		
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] inputValues = { 10, 20, 20 };
+		char[] inputValues = new char[]{'.', '.', '.', '.' };
 		boolean result = check(inputValues);
 		if (!result) {
 			Symbolic.printPC("Not Accepted");
